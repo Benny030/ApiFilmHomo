f99e6658fa8ef620583969a5f32df606b2fe9816
Benny030 <spaolo.bennardo@itis.pr.it>
2023-05-18 09:54:12 +0200

    PROVA

--- indexOLD.js
+++ indexOLD.js
@@ -1,15 +1,15 @@
-//prima------
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+import express from 'express';
+const fs = require("fs");
+const path = require("path");
+
 const app = express();
-const PORT = 8080;
-//seconda------
+const port = 3000;
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(
-  '/static',
-  express.static(path.join(__dirname, 'public')),
-);
+app.use( '/static', express.static(__dirname));
+
+
 
 app.get('/', (req, res) => {
     res.send('Il server sta funzionando correttamente. \n Per visuallizzare tutti i dati basta andare al seguente ip: http://localhost:3000/films')
@@ -89,27 +89,22 @@ app.post('/film/:id', (req, res) => {
     res.send('Modifica avvenuta correttamente')
 })
 
-
-fs.open("public/dati.json", "wx+", (err, f) => {
+//LETTURA FILE 
+fs.open("/dati.json", "wx+", (err, f) => {
     if (err) {
-        fs.readFile("public/dati.json", (erro, data) => {
+        fs.readFile("/dati.json", (erro, data) => {
             if (erro) console.error(erro);
             else {
-                console.log("Read!");
+                console.log("Letto!");
                 films = JSON.parse(data);
             }
         });
         return;
     }
-    fs.writeFile("public/dati.json", JSON.stringify(films), (err) => {
-        if (err) console.error(err);
-        else console.log("Saved!");
-    });
+        fs.writeFile("/dati.json", JSON.stringify(films), (err) => {
+            if (err) console.error(err);
+            else console.log("File FILM Salvato!")
+        });
 });
 
-
-
-app.listen(
-    PORT,
-    () => console.log("it'alive on http://localhost:8080")
-)
+app.listen(port, () => console.log("L'app sta funzionando"));
