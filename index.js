9f4ee8f0447c02b0185bc6f62cc1c20c2f117496
Benny030 <spaolo.bennardo@itis.pr.it>
2023-05-18 10:38:48 +0200

    d

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 import express from 'express';
 import fs from 'fs';
 import path from 'path';
-
+import fileURLToPath from 'url';
 
 const app = express();
 const PORT = 8080;
 
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.json());
