1fc338a89b11e660bd3268506185cdef7968d435
Benny030 <spaolo.bennardo@itis.pr.it>
2023-05-18 11:28:20 +0200

    no cors

--- index.js
+++ index.js
@@ -3,11 +3,6 @@ import express from 'express';
 import fs from 'fs';
 import path from 'path';
 import url from 'url';
-import cors from 'cors';
-
-const corsOptions = {
-  origin: "https://api-film-homo.vercel.app/films",
-};
 
 
 const app = express();
@@ -16,7 +11,6 @@ const PORT = 8080;
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(cors(corsOptions));
 app.use(express.json());
 app.use(
   '/static',
